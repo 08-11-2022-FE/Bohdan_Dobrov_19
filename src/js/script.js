63510bf1d5c66dc04549cb6ccaa54f3d0ada75e8
Bohdan Dobrov <bov.don.94@gmail.com>
2022-12-26 20:58:09 +0200

    added function for small device

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
+// funtion changes menu size
 function sidebar() {
+    // variable for change menu size
     var x = document.getElementById("sidebar");
     if (x.classList.contains("sidebar-hidden")) {
         x.classList.add("sidebar-visible");
@@ -8,3 +10,22 @@ function sidebar() {
         x.classList.remove("sidebar-visible");
     }
 }
+
+// function changes menu size if device width is small
+
+function myFunction(width) {
+    var x = document.getElementById("sidebar");
+    if (width.matches) { // If media query matches
+        x.classList.add("sidebar-hidden");
+        x.classList.remove("sidebar-visible");
+    } else {
+        x.classList.add("sidebar-visible");
+        x.classList.remove("sidebar-hidden");
+    }
+  }
+
+  var width = window.matchMedia("(max-width: 550px)");
+  width.addListener(myFunction); 
+  myFunction(width);
+
+//function changes themes(dark/light)
