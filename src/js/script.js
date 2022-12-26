45c92d197c01d36abdb72040644f8883221f820f
Bohdan Dobrov <bov.don.94@gmail.com>
2022-12-26 15:45:18 +0200

    added homework

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+function sidebar() {
+    var x = document.getElementById("sidebar");
+    if (x.classList.contains("sidebar-hidden")) {
+        x.classList.add("sidebar-visible");
+        x.classList.remove("sidebar-hidden");
+    } else {
+        x.classList.add("sidebar-hidden");
+        x.classList.remove("sidebar-visible");
+    }
+}
